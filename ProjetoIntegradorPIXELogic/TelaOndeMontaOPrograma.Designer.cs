320f4f728bdf5a359fcf3eab62662a93231b3d49
MaiconConstancio <135743035+MaiconConstancio@users.noreply.github.com>
2023-08-30 15:19:45 -0300

    Coloquei a tela "TelaOndeMontarOPrograma"

---
+++ ProjetoIntegradorPIXELogic/TelaOndeMontaOPrograma.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace ProjetoIntegradorPIXELogic
+{
+    partial class TelaOndeMontaOPrograma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblSejaBemVindo = new Label();
+            panel1 = new Panel();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            listBox5 = new ListBox();
+            listBox6 = new ListBox();
+            listBox7 = new ListBox();
+            listBox8 = new ListBox();
+            listBox9 = new ListBox();
+            listBox10 = new ListBox();
+            listBox11 = new ListBox();
+            listBox12 = new ListBox();
+            listBox13 = new ListBox();
+            listBox14 = new ListBox();
+            listBox15 = new ListBox();
+            listBox16 = new ListBox();
+            listBox17 = new ListBox();
+            listBox18 = new ListBox();
+            listBox19 = new ListBox();
+            listBox20 = new ListBox();
+            listBox21 = new ListBox();
+            listBox22 = new ListBox();
+            listBox23 = new ListBox();
+            listBox24 = new ListBox();
+            btnAvançar = new WiLBiT.WiLBiTButton2();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblSejaBemVindo
+            // 
+            lblSejaBemVindo.AutoSize = true;
+            lblSejaBemVindo.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSejaBemVindo.ForeColor = Color.FromArgb(160, 245, 255);
+            lblSejaBemVindo.Location = new Point(107, 24);
+            lblSejaBemVindo.Name = "lblSejaBemVindo";
+            lblSejaBemVindo.Size = new Size(561, 37);
+            lblSejaBemVindo.TabIndex = 15;
+            lblSejaBemVindo.Text = "Escolha quais ferramentas são necessárias!";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(listBox13);
+            panel1.Controls.Add(listBox14);
+            panel1.Controls.Add(listBox15);
+            panel1.Controls.Add(listBox16);
+            panel1.Controls.Add(listBox17);
+            panel1.Controls.Add(listBox18);
+            panel1.Controls.Add(listBox19);
+            panel1.Controls.Add(listBox20);
+            panel1.Controls.Add(listBox21);
+            panel1.Controls.Add(listBox22);
+            panel1.Controls.Add(listBox23);
+            panel1.Controls.Add(listBox24);
+            panel1.Controls.Add(listBox10);
+            panel1.Controls.Add(listBox11);
+            panel1.Controls.Add(listBox12);
+            panel1.Controls.Add(listBox7);
+            panel1.Controls.Add(listBox8);
+            panel1.Controls.Add(listBox9);
+            panel1.Controls.Add(listBox4);
+            panel1.Controls.Add(listBox5);
+            panel1.Controls.Add(listBox6);
+            panel1.Controls.Add(listBox3);
+            panel1.Controls.Add(listBox2);
+            panel1.Controls.Add(listBox1);
+            panel1.Location = new Point(25, 64);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(739, 312);
+            panel1.TabIndex = 16;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(17, 14);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(17, 114);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(120, 94);
+            listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(17, 214);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(120, 94);
+            listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 15;
+            listBox4.Location = new Point(143, 214);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(120, 94);
+            listBox4.TabIndex = 5;
+            // 
+            // listBox5
+            // 
+            listBox5.FormattingEnabled = true;
+            listBox5.ItemHeight = 15;
+            listBox5.Location = new Point(143, 114);
+            listBox5.Name = "listBox5";
+            listBox5.Size = new Size(120, 94);
+            listBox5.TabIndex = 4;
+            // 
+            // listBox6
+            // 
+            listBox6.FormattingEnabled = true;
+            listBox6.ItemHeight = 15;
+            listBox6.Location = new Point(143, 14);
+            listBox6.Name = "listBox6";
+            listBox6.Size = new Size(120, 94);
+            listBox6.TabIndex = 3;
+            // 
+            // listBox7
+            // 
+            listBox7.FormattingEnabled = true;
+            listBox7.ItemHeight = 15;
+            listBox7.Location = new Point(269, 214);
+            listBox7.Name = "listBox7";
+            listBox7.Size = new Size(120, 94);
+            listBox7.TabIndex = 8;
+            // 
+            // listBox8
+            // 
+            listBox8.FormattingEnabled = true;
+            listBox8.ItemHeight = 15;
+            listBox8.Location = new Point(269, 114);
+            listBox8.Name = "listBox8";
+            listBox8.Size = new Size(120, 94);
+            listBox8.TabIndex = 7;
+            // 
+            // listBox9
+            // 
+            listBox9.FormattingEnabled = true;
+            listBox9.ItemHeight = 15;
+            listBox9.Location = new Point(269, 14);
+            listBox9.Name = "listBox9";
+            listBox9.Size = new Size(120, 94);
+            listBox9.TabIndex = 6;
+            // 
+            // listBox10
+            // 
+            listBox10.FormattingEnabled = true;
+            listBox10.ItemHeight = 15;
+            listBox10.Location = new Point(395, 214);
+            listBox10.Name = "listBox10";
+            listBox10.Size = new Size(120, 94);
+            listBox10.TabIndex = 11;
+            // 
+            // listBox11
+            // 
+            listBox11.FormattingEnabled = true;
+            listBox11.ItemHeight = 15;
+            listBox11.Location = new Point(395, 114);
+            listBox11.Name = "listBox11";
+            listBox11.Size = new Size(120, 94);
+            listBox11.TabIndex = 10;
+            // 
+            // listBox12
+            // 
+            listBox12.FormattingEnabled = true;
+            listBox12.ItemHeight = 15;
+            listBox12.Location = new Point(395, 14);
+            listBox12.Name = "listBox12";
+            listBox12.Size = new Size(120, 94);
+            listBox12.TabIndex = 9;
+            // 
+            // listBox13
+            // 
+            listBox13.FormattingEnabled = true;
+            listBox13.ItemHeight = 15;
+            listBox13.Location = new Point(901, 214);
+            listBox13.Name = "listBox13";
+            listBox13.Size = new Size(120, 94);
+            listBox13.TabIndex = 23;
+            // 
+            // listBox14
+            // 
+            listBox14.FormattingEnabled = true;
+            listBox14.ItemHeight = 15;
+            listBox14.Location = new Point(901, 114);
+            listBox14.Name = "listBox14";
+            listBox14.Size = new Size(120, 94);
+            listBox14.TabIndex = 22;
+            // 
+            // listBox15
+            // 
+            listBox15.FormattingEnabled = true;
+            listBox15.ItemHeight = 15;
+            listBox15.Location = new Point(901, 14);
+            listBox15.Name = "listBox15";
+            listBox15.Size = new Size(120, 94);
+            listBox15.TabIndex = 21;
+            // 
+            // listBox16
+            // 
+            listBox16.FormattingEnabled = true;
+            listBox16.ItemHeight = 15;
+            listBox16.Location = new Point(775, 214);
+            listBox16.Name = "listBox16";
+            listBox16.Size = new Size(120, 94);
+            listBox16.TabIndex = 20;
+            // 
+            // listBox17
+            // 
+            listBox17.FormattingEnabled = true;
+            listBox17.ItemHeight = 15;
+            listBox17.Location = new Point(775, 114);
+            listBox17.Name = "listBox17";
+            listBox17.Size = new Size(120, 94);
+            listBox17.TabIndex = 19;
+            // 
+            // listBox18
+            // 
+            listBox18.FormattingEnabled = true;
+            listBox18.ItemHeight = 15;
+            listBox18.Location = new Point(775, 14);
+            listBox18.Name = "listBox18";
+            listBox18.Size = new Size(120, 94);
+            listBox18.TabIndex = 18;
+            // 
+            // listBox19
+            // 
+            listBox19.FormattingEnabled = true;
+            listBox19.ItemHeight = 15;
+            listBox19.Location = new Point(649, 214);
+            listBox19.Name = "listBox19";
+            listBox19.Size = new Size(120, 94);
+            listBox19.TabIndex = 17;
+            // 
+            // listBox20
+            // 
+            listBox20.FormattingEnabled = true;
+            listBox20.ItemHeight = 15;
+            listBox20.Location = new Point(649, 114);
+            listBox20.Name = "listBox20";
+            listBox20.Size = new Size(120, 94);
+            listBox20.TabIndex = 16;
+            // 
+            // listBox21
+            // 
+            listBox21.FormattingEnabled = true;
+            listBox21.ItemHeight = 15;
+            listBox21.Location = new Point(649, 14);
+            listBox21.Name = "listBox21";
+            listBox21.Size = new Size(120, 94);
+            listBox21.TabIndex = 15;
+            // 
+            // listBox22
+            // 
+            listBox22.FormattingEnabled = true;
+            listBox22.ItemHeight = 15;
+            listBox22.Location = new Point(523, 214);
+            listBox22.Name = "listBox22";
+            listBox22.Size = new Size(120, 94);
+            listBox22.TabIndex = 14;
+            // 
+            // listBox23
+            // 
+            listBox23.FormattingEnabled = true;
+            listBox23.ItemHeight = 15;
+            listBox23.Location = new Point(523, 114);
+            listBox23.Name = "listBox23";
+            listBox23.Size = new Size(120, 94);
+            listBox23.TabIndex = 13;
+            // 
+            // listBox24
+            // 
+            listBox24.FormattingEnabled = true;
+            listBox24.ItemHeight = 15;
+            listBox24.Location = new Point(523, 14);
+            listBox24.Name = "listBox24";
+            listBox24.Size = new Size(120, 94);
+            listBox24.TabIndex = 12;
+            // 
+            // btnAvançar
+            // 
+            btnAvançar.BackColor = Color.FromArgb(72, 116, 245);
+            btnAvançar.BackgroundColor = Color.FromArgb(72, 116, 245);
+            btnAvançar.BorderColor = Color.FromArgb(0, 242, 254);
+            btnAvançar.BorderRadius = 18;
+            btnAvançar.BorderSize = 2;
+            btnAvançar.FlatAppearance.BorderSize = 0;
+            btnAvançar.FlatStyle = FlatStyle.Flat;
+            btnAvançar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAvançar.ForeColor = Color.Black;
+            btnAvançar.Location = new Point(350, 393);
+            btnAvançar.Name = "btnAvançar";
+            btnAvançar.Size = new Size(120, 36);
+            btnAvançar.TabIndex = 17;
+            btnAvançar.Text = "SALVAR";
+            btnAvançar.TextColor = Color.Black;
+            btnAvançar.UseVisualStyleBackColor = false;
+            // 
+            // TelaOndeMontaOPrograma
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(16, 25, 56);
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnAvançar);
+            Controls.Add(panel1);
+            Controls.Add(lblSejaBemVindo);
+            Name = "TelaOndeMontaOPrograma";
+            Text = "TelaOndeMontaOPrograma";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblSejaBemVindo;
+        private Panel panel1;
+        private ListBox listBox13;
+        private ListBox listBox14;
+        private ListBox listBox15;
+        private ListBox listBox16;
+        private ListBox listBox17;
+        private ListBox listBox18;
+        private ListBox listBox19;
+        private ListBox listBox20;
+        private ListBox listBox21;
+        private ListBox listBox22;
+        private ListBox listBox23;
+        private ListBox listBox24;
+        private ListBox listBox10;
+        private ListBox listBox11;
+        private ListBox listBox12;
+        private ListBox listBox7;
+        private ListBox listBox8;
+        private ListBox listBox9;
+        private ListBox listBox4;
+        private ListBox listBox5;
+        private ListBox listBox6;
+        private ListBox listBox3;
+        private ListBox listBox2;
+        private ListBox listBox1;
+        private WiLBiT.WiLBiTButton2 btnAvançar;
+    }
+}
