5ca0c8c2fa4ee0214821a67d234b3fcd85d7dbca
JoaoVictorBento <joao.vbsantos37@senacsp.edu.br>
2023-08-31 21:32:18 -0300

    Form1 concluido

---
+++ ProjetoIntegradorPIXELogic/Conexao.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorPIXELlogic
+{
+    public class Conexao
+    {
+
+        const string host = "localhost";
+        const string banco = "pixellogic_pi";
+        const string usuario = "root";
+        const string senha = "";
+
+        public static MySqlConnection connection = new MySqlConnection($"Server={host};Database={banco};Uid={usuario};Pwd={senha};");
+
+        public static DataTable executaQuery(string query)
+        {
+
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader read = command.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(read);
+                return dataTable;
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("Erro ao realizar consulta:");
+                Console.WriteLine(erro.Message);
+                return null;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+
+        }
+    
+    }
+}
---
+++ ProjetoIntegradorPIXELogic/Form1.cs
@@ -0,0 +1,62 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProjetoIntegradorPIXELlogic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnEntra_Click(object sender, EventArgs e)
+        {
+
+            if(Auxiliares.verificaCampo("email", txtLogin) == false &&
+            Auxiliares.verificaCampo("senha", txtSenha) == false)
+            {
+
+                if (Auxiliares.existe("email", txtLogin) == true)
+                {
+
+                    if (Auxiliares.existe("senha", txtSenha) == true)
+                    {
+
+                        string admin = $"select email,admin from usuarios where email = '{txtLogin.Text}' and admin = true;";
+
+                        if (Conexao.executaQuery(admin).Rows.Count > 0)
+                        {
+
+                            Form1 form = new Form1();
+                            form.Show();
+
+                        }
+
+                        else
+                        {
+
+                            Form1 form = new Form1();
+                            form.Show();
+                            /*digite o form*/
+
+                        }
+
+                    }
+
+                    else { MessageBox.Show($" Por favor digite uma senha válida!", $"Senha inválida!", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+                }
+
+                else { MessageBox.Show($" Por favor digite um login válido!", $"Login inválido!", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+            }
+            
+ 
+
+        }
+
+    }
+
+}
