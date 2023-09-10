f6027fa203879a94fba695d5b09cf75f252f2251
JOAO VICTOR BENTO DOS SANTOS <joao.vbsantos37@senacsp.edu.br>
2023-09-10 00:03:44 -0300

    recomeço

--- ProjetoIntegradorPIXELogic/Form1.Designer.cs
+++ ProjetoIntegradorPIXELogic/Form1.Designer.cs
@@ -1,14 +1,14 @@
-namespace ProjetoIntegradorPIXELogic
+﻿namespace ProjetoIntegradorPIXELogic
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,120 +23,17 @@ namespace ProjetoIntegradorPIXELogic
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            txtLogin = new TextBox();
-            txtSenha = new TextBox();
-            btnEntra = new Button();
-            pictureEscrita = new PictureBox();
-            pictureLogo = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureEscrita).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureLogo).BeginInit();
-            SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.FromArgb(63, 136, 254);
-            label1.Location = new Point(533, 160);
-            label1.Name = "label1";
-            label1.Size = new Size(37, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Login";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.ForeColor = Color.FromArgb(63, 136, 254);
-            label2.Location = new Point(533, 226);
-            label2.Name = "label2";
-            label2.Size = new Size(41, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Senha";
-            // 
-            // txtLogin
-            // 
-            txtLogin.BackColor = Color.FromArgb(53, 81, 142);
-            txtLogin.Location = new Point(501, 178);
-            txtLogin.Name = "txtLogin";
-            txtLogin.Size = new Size(100, 23);
-            txtLogin.TabIndex = 2;
-            // 
-            // txtSenha
-            // 
-            txtSenha.BackColor = Color.FromArgb(53, 81, 142);
-            txtSenha.Location = new Point(501, 244);
-            txtSenha.Name = "txtSenha";
-            txtSenha.Size = new Size(100, 23);
-            txtSenha.TabIndex = 3;
-            // 
-            // btnEntra
-            // 
-            btnEntra.BackColor = Color.FromArgb(0, 242, 254);
-            btnEntra.FlatStyle = FlatStyle.Flat;
-            btnEntra.Location = new Point(501, 300);
-            btnEntra.Name = "btnEntra";
-            btnEntra.Size = new Size(100, 23);
-            btnEntra.TabIndex = 4;
-            btnEntra.Text = "ENTRAR";
-            btnEntra.UseVisualStyleBackColor = false;
-            btnEntra.Click += btnEntra_Click;
-            // 
-            // pictureEscrita
-            // 
-            pictureEscrita.BackColor = Color.Lime;
-            pictureEscrita.Location = new Point(148, 26);
-            pictureEscrita.Name = "pictureEscrita";
-            pictureEscrita.Size = new Size(426, 72);
-            pictureEscrita.TabIndex = 5;
-            pictureEscrita.TabStop = false;
-            // 
-            // pictureLogo
-            // 
-            pictureLogo.BackColor = Color.Lime;
-            pictureLogo.Location = new Point(65, 188);
-            pictureLogo.Name = "pictureLogo";
-            pictureLogo.Size = new Size(156, 135);
-            pictureLogo.TabIndex = 6;
-            pictureLogo.TabStop = false;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(6, 10, 22);
-            ClientSize = new Size(659, 373);
-            Controls.Add(pictureLogo);
-            Controls.Add(pictureEscrita);
-            Controls.Add(btnEntra);
-            Controls.Add(txtSenha);
-            Controls.Add(txtLogin);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureEscrita).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureLogo).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
-
-        private Label label1;
-        private Label label2;
-        private TextBox txtLogin;
-        private TextBox txtSenha;
-        private Button btnEntra;
-        private PictureBox pictureEscrita;
-        private PictureBox pictureLogo;
     }
 }
--- ProjetoIntegradorPIXELogic/Form1.cs
+++ ProjetoIntegradorPIXELogic/Form1.cs
@@ -1,5 +1,12 @@
-using MySql.Data.MySqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProjetoIntegradorPIXELogic
 {
@@ -9,94 +16,5 @@ namespace ProjetoIntegradorPIXELogic
         {
             InitializeComponent();
         }
-
-        private void btnEntra_Click(object sender, EventArgs e)
-        {
-
-            if (Auxiliares.verificaCampo("email", txtLogin) == false &&
-            Auxiliares.verificaCampo("senha", txtSenha) == false)
-            {
-
-                if (Auxiliares.existe("usuarios","email", txtLogin) == true)
-                {
-
-                    if (Auxiliares.existe("usuarios","senha", txtSenha) == true)
-                    {
-
-                        string admin = $"select email,admin from usuarios where email = '{txtLogin.Text}' and admin = true;";
-
-                        if (Conexao.executaQuery(admin).Rows.Count > 0)
-                        {
-
-                            string verifica = $"select * from usuarios where email = '{txtLogin.Text}' and recriouSenha = false;";
-
-                            if(Conexao.executaQuery(verifica).Rows.Count > 0)
-                            {
-
-                                Form2 form2 = new Form2();
-                                form2.armazenamentoLogin = txtLogin.Text;
-                                form2.armazenamentoSenha = txtSenha.Text;
-                                form2.Show();
-                                this.Hide();
-
-                            }
-                            
-                            else
-                            {
-
-                                Form3 form3 = new Form3();
-                                form3.Show();
-                                this.Hide();
-
-                            }
-
-                        }
-
-                        else
-                        {
-
-                            string verifica2 = $"select * from usuarios where email = '{txtLogin.Text}' and recriouSenha = false;";
-
-                            if (Conexao.executaQuery(verifica2).Rows.Count > 0)
-                            {
-
-                                Form2 form2 = new Form2();
-                                form2.armazenamentoLogin = txtLogin.Text;
-                                form2.armazenamentoSenha = txtSenha.Text;
-                                form2.Show();
-                                this.Hide();
-
-                            }
-
-                            else
-                            {
-
-                                Form5 form5 = new Form5();
-                                form5.Show();
-                                this.Hide();
-
-                            }
-
-                        }
-
-                    }
-
-                    else { MessageBox.Show($" Por favor digite uma senha v�lida!", $"Senha inv�lida!", MessageBoxButtons.OK, MessageBoxIcon.Error); }
-
-                }
-
-                else { MessageBox.Show($" Por favor digite um login v�lido!", $"Login inv�lido!", MessageBoxButtons.OK, MessageBoxIcon.Error); }
-
-            }
-
-
-
-        }
-        
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
-
 }
