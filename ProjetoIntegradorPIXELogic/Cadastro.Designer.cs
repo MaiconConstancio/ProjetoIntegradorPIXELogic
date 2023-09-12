67596cac1d3e512998abfa6459da5b9b30988a58
JoaoVictorBento <joao.vbsantos37@senacsp.edu.br>
2023-09-11 21:18:45 -0300

    Função fornecedores pronta, só falta alguns detalhes bobos

--- ProjetoIntegradorPIXELogic/Cadastro.Designer.cs
+++ ProjetoIntegradorPIXELogic/Cadastro.Designer.cs
@@ -32,18 +32,24 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
-            txtSenha = new TextBox();
-            txtLogin = new TextBox();
-            txtCargo = new TextBox();
+            txtCNPJ = new TextBox();
+            txtTelefone = new TextBox();
+            txtEndereco = new TextBox();
             txtNome = new TextBox();
             button1 = new Button();
             button2 = new Button();
+            txtCEP = new TextBox();
+            label5 = new Label();
+            txtNumero = new TextBox();
+            label6 = new Label();
+            txtCidade = new TextBox();
+            label7 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(337, 108);
+            label1.Location = new Point(328, 30);
             label1.Name = "label1";
             label1.Size = new Size(40, 15);
             label1.TabIndex = 0;
@@ -52,54 +58,54 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(337, 154);
+            label2.Location = new Point(328, 76);
             label2.Name = "label2";
-            label2.Size = new Size(39, 15);
+            label2.Size = new Size(56, 15);
             label2.TabIndex = 1;
-            label2.Text = "Cargo";
+            label2.Text = "Endereço";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(337, 218);
+            label3.Location = new Point(328, 140);
             label3.Name = "label3";
-            label3.Size = new Size(37, 15);
+            label3.Size = new Size(51, 15);
             label3.TabIndex = 2;
-            label3.Text = "Login";
+            label3.Text = "Telefone";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(352, 273);
+            label4.Location = new Point(343, 195);
             label4.Name = "label4";
-            label4.Size = new Size(39, 15);
+            label4.Size = new Size(34, 15);
             label4.TabIndex = 3;
-            label4.Text = "Senha";
+            label4.Text = "CNPJ";
             // 
-            // txtSenha
+            // txtCNPJ
             // 
-            txtSenha.Location = new Point(314, 291);
-            txtSenha.Name = "txtSenha";
-            txtSenha.Size = new Size(100, 23);
-            txtSenha.TabIndex = 4;
+            txtCNPJ.Location = new Point(305, 213);
+            txtCNPJ.Name = "txtCNPJ";
+            txtCNPJ.Size = new Size(100, 23);
+            txtCNPJ.TabIndex = 4;
             // 
-            // txtLogin
+            // txtTelefone
             // 
-            txtLogin.Location = new Point(314, 236);
-            txtLogin.Name = "txtLogin";
-            txtLogin.Size = new Size(100, 23);
-            txtLogin.TabIndex = 5;
+            txtTelefone.Location = new Point(305, 158);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(100, 23);
+            txtTelefone.TabIndex = 5;
             // 
-            // txtCargo
+            // txtEndereco
             // 
-            txtCargo.Location = new Point(314, 172);
-            txtCargo.Name = "txtCargo";
-            txtCargo.Size = new Size(100, 23);
-            txtCargo.TabIndex = 6;
+            txtEndereco.Location = new Point(305, 94);
+            txtEndereco.Name = "txtEndereco";
+            txtEndereco.Size = new Size(100, 23);
+            txtEndereco.TabIndex = 6;
             // 
             // txtNome
             // 
-            txtNome.Location = new Point(314, 126);
+            txtNome.Location = new Point(305, 48);
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(100, 23);
             txtNome.TabIndex = 7;
@@ -124,17 +130,71 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // txtCEP
+            // 
+            txtCEP.Location = new Point(305, 272);
+            txtCEP.Name = "txtCEP";
+            txtCEP.Size = new Size(100, 23);
+            txtCEP.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(343, 254);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 15);
+            label5.TabIndex = 10;
+            label5.Text = "CEP";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(305, 337);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(100, 23);
+            txtNumero.TabIndex = 13;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(343, 319);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Numero";
+            // 
+            // txtCidade
+            // 
+            txtCidade.Location = new Point(305, 399);
+            txtCidade.Name = "txtCidade";
+            txtCidade.Size = new Size(100, 23);
+            txtCidade.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(343, 381);
+            label7.Name = "label7";
+            label7.Size = new Size(44, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Cidade";
+            // 
             // Cadastro
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(txtCidade);
+            Controls.Add(label7);
+            Controls.Add(txtNumero);
+            Controls.Add(label6);
+            Controls.Add(txtCEP);
+            Controls.Add(label5);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(txtNome);
-            Controls.Add(txtCargo);
-            Controls.Add(txtLogin);
-            Controls.Add(txtSenha);
+            Controls.Add(txtEndereco);
+            Controls.Add(txtTelefone);
+            Controls.Add(txtCNPJ);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -152,11 +212,17 @@
         private Label label2;
         private Label label3;
         private Label label4;
-        private TextBox txtSenha;
-        private TextBox txtLogin;
-        private TextBox txtCargo;
+        private TextBox txtCNPJ;
+        private TextBox txtTelefone;
+        private TextBox txtEndereco;
         private TextBox txtNome;
         private Button button1;
         private Button button2;
+        private TextBox txtCEP;
+        private Label label5;
+        private TextBox txtNumero;
+        private Label label6;
+        private TextBox txtCidade;
+        private Label label7;
     }
 }
