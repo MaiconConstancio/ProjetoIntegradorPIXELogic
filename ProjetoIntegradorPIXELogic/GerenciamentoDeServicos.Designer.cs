dede0e172c6f4d14313825511e91fdf3de4881ea
IELLEN DA SILVA DANAGA <iellen.sdanaga@senacsp.edu.br>
2023-09-12 15:12:09 -0300

    metade da tela GerenciamentoDeServicos

--- ProjetoIntegradorPIXELogic/GerenciamentoDeServicos.Designer.cs
+++ ProjetoIntegradorPIXELogic/GerenciamentoDeServicos.Designer.cs
@@ -30,6 +30,12 @@
         {
             wilBitButton21 = new WiLBiT.WiLBiTButton2();
             wilBitButton23 = new WiLBiT.WiLBiTButton2();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
             // 
             // wilBitButton21
@@ -70,23 +76,101 @@
             wilBitButton23.TextColor = Color.Black;
             wilBitButton23.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(63, 136, 254);
+            label1.Location = new Point(23, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 32);
+            label1.TabIndex = 26;
+            label1.Text = "Cliente";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(63, 136, 254);
+            label2.Location = new Point(621, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 32);
+            label2.TabIndex = 27;
+            label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(63, 136, 254);
+            label3.Location = new Point(474, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 32);
+            label3.TabIndex = 28;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(63, 136, 254);
+            label4.Location = new Point(332, 92);
+            label4.Name = "label4";
+            label4.Size = new Size(120, 32);
+            label4.TabIndex = 29;
+            label4.Text = "Endereço";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(63, 136, 254);
+            label5.Location = new Point(253, 92);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 32);
+            label5.TabIndex = 30;
+            label5.Text = "Valor";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.FromArgb(63, 136, 254);
+            label6.Location = new Point(136, 92);
+            label6.Name = "label6";
+            label6.Size = new Size(98, 32);
+            label6.TabIndex = 31;
+            label6.Text = "Serviço";
+            // 
             // GerenciamentoDeServicos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(16, 25, 56);
             ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(wilBitButton23);
             Controls.Add(wilBitButton21);
             FormBorderStyle = FormBorderStyle.None;
             Name = "GerenciamentoDeServicos";
             Text = "GerenciamentoDeServicos";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private WiLBiT.WiLBiTButton2 wilBitButton21;
         private WiLBiT.WiLBiTButton2 wilBitButton23;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
