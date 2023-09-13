2dbe7ba7892259b1ce2f643129f3a5f00762c1f0
EDUARDO DE LIRA SILVA <eduardo.lsilva43@senacsp.edu.br>
2023-09-13 16:15:22 -0300

    Alteração cadastro de clientes

--- ProjetoIntegradorPIXELogic/ListaDeFornecedores.Designer.cs
+++ ProjetoIntegradorPIXELogic/ListaDeFornecedores.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ListaDeFornecedores";
+            lblTelefone = new Label();
+            lblCidade = new Label();
+            lblNumero = new Label();
+            lblEditar = new Label();
+            lblCEP = new Label();
+            lblEndereço = new Label();
+            lblNome = new Label();
+            label8 = new Label();
+            lblCNPJ = new Label();
+            btnSalvar = new WiLBiT.WiLBiTButton2();
+            SuspendLayout();
+            // 
+            // lblTelefone
+            // 
+            lblTelefone.AutoSize = true;
+            lblTelefone.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTelefone.ForeColor = Color.FromArgb(63, 136, 254);
+            lblTelefone.Location = new Point(210, 47);
+            lblTelefone.Name = "lblTelefone";
+            lblTelefone.Size = new Size(84, 25);
+            lblTelefone.TabIndex = 15;
+            lblTelefone.Text = "Telefone";
+            // 
+            // lblCidade
+            // 
+            lblCidade.AutoSize = true;
+            lblCidade.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCidade.ForeColor = Color.FromArgb(63, 136, 254);
+            lblCidade.Location = new Point(603, 47);
+            lblCidade.Name = "lblCidade";
+            lblCidade.Size = new Size(71, 25);
+            lblCidade.TabIndex = 14;
+            lblCidade.Text = "Cidade";
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblNumero.ForeColor = Color.FromArgb(63, 136, 254);
+            lblNumero.Location = new Point(488, 47);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(83, 25);
+            lblNumero.TabIndex = 13;
+            lblNumero.Text = "Numero";
+            // 
+            // lblEditar
+            // 
+            lblEditar.AutoSize = true;
+            lblEditar.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblEditar.ForeColor = Color.FromArgb(63, 136, 254);
+            lblEditar.Location = new Point(715, 47);
+            lblEditar.Name = "lblEditar";
+            lblEditar.Size = new Size(62, 25);
+            lblEditar.TabIndex = 12;
+            lblEditar.Text = "Editar";
+            // 
+            // lblCEP
+            // 
+            lblCEP.AutoSize = true;
+            lblCEP.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCEP.ForeColor = Color.FromArgb(63, 136, 254);
+            lblCEP.Location = new Point(407, 47);
+            lblCEP.Name = "lblCEP";
+            lblCEP.Size = new Size(45, 25);
+            lblCEP.TabIndex = 11;
+            lblCEP.Text = "CEP";
+            // 
+            // lblEndereço
+            // 
+            lblEndereço.AutoSize = true;
+            lblEndereço.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblEndereço.ForeColor = Color.FromArgb(63, 136, 254);
+            lblEndereço.Location = new Point(107, 47);
+            lblEndereço.Name = "lblEndereço";
+            lblEndereço.Size = new Size(91, 25);
+            lblEndereço.TabIndex = 10;
+            lblEndereço.Text = "Endereço";
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblNome.ForeColor = Color.FromArgb(63, 136, 254);
+            lblNome.Location = new Point(25, 47);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(65, 25);
+            lblNome.TabIndex = 9;
+            lblNome.Text = "Nome";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.FromArgb(63, 136, 254);
+            label8.Location = new Point(365, 215);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 21);
+            label8.TabIndex = 16;
+            // 
+            // lblCNPJ
+            // 
+            lblCNPJ.AutoSize = true;
+            lblCNPJ.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCNPJ.ForeColor = Color.FromArgb(63, 136, 254);
+            lblCNPJ.Location = new Point(319, 47);
+            lblCNPJ.Name = "lblCNPJ";
+            lblCNPJ.Size = new Size(57, 25);
+            lblCNPJ.TabIndex = 17;
+            lblCNPJ.Text = "CNPJ";
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.BackColor = Color.FromArgb(72, 116, 245);
+            btnSalvar.BackgroundColor = Color.FromArgb(72, 116, 245);
+            btnSalvar.BorderColor = Color.FromArgb(0, 242, 254);
+            btnSalvar.BorderRadius = 19;
+            btnSalvar.BorderSize = 2;
+            btnSalvar.FlatAppearance.BorderSize = 0;
+            btnSalvar.FlatStyle = FlatStyle.Flat;
+            btnSalvar.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSalvar.ForeColor = Color.Black;
+            btnSalvar.Location = new Point(25, 398);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(150, 40);
+            btnSalvar.TabIndex = 18;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.TextColor = Color.Black;
+            btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // ListaDeFornecedores
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(16, 25, 56);
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalvar);
+            Controls.Add(lblCNPJ);
+            Controls.Add(label8);
+            Controls.Add(lblTelefone);
+            Controls.Add(lblCidade);
+            Controls.Add(lblNumero);
+            Controls.Add(lblEditar);
+            Controls.Add(lblCEP);
+            Controls.Add(lblEndereço);
+            Controls.Add(lblNome);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ListaDeFornecedores";
+            Text = "ListaDeFornecedores";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTelefone;
+        private Label lblCidade;
+        private Label lblNumero;
+        private Label lblEditar;
+        private Label lblCEP;
+        private Label lblEndereço;
+        private Label lblNome;
+        private Label label8;
+        private Label lblCNPJ;
+        private WiLBiT.WiLBiTButton2 btnSalvar;
     }
 }
