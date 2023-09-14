29546e8908830ca8ea9568e7554c7ca8c2948ccc
MaiconConstancio <135743035+MaiconConstancio@users.noreply.github.com>
2023-09-14 16:49:21 -0300

    Criei a tela PalcoListaDeProdutos

---
+++ ProjetoIntegradorPIXELogic/PalcoListaDeProdutos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoIntegradorPIXELogic
+{
+    public partial class PalcoListaDeProdutos : Form
+    {
+        public PalcoListaDeProdutos()
+        {
+            InitializeComponent();
+        }
+
+        private void PalcoListaDeProdutos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
