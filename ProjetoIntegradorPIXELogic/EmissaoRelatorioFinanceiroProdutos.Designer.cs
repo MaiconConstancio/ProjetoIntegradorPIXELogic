7cccc93b06534d8c266977dc6d28cea16e57d2e0
IellenSilva <135743068+IellenSilva@users.noreply.github.com>
2023-09-14 22:56:52 -0300

    criei a tela EmissaoRelatorioFinanceiroProdutos

---
+++ ProjetoIntegradorPIXELogic/EmissaoRelatorioFinanceiroProdutos.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ProjetoIntegradorPIXELogic
+{
+    partial class EmissaoRelatorioFinanceiroProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            lblAcao = new Label();
+            label1 = new Label();
+            lblValor = new Label();
+            lblCliente = new Label();
+            lblProduto = new Label();
+            btnVoltar = new WiLBiT.WiLBiTButton2();
+            btnImprimir = new WiLBiT.WiLBiTButton2();
+            btnSalvar = new WiLBiT.WiLBiTButton2();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.ESCRITA_SEM_Fundo9;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(307, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lblAcao
+            // 
+            lblAcao.AutoSize = true;
+            lblAcao.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblAcao.ForeColor = Color.FromArgb(63, 136, 254);
+            lblAcao.Location = new Point(687, 101);
+            lblAcao.Name = "lblAcao";
+            lblAcao.Size = new Size(56, 25);
+            lblAcao.TabIndex = 29;
+            lblAcao.Text = "Ação";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(63, 136, 254);
+            label1.Location = new Point(520, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 25);
+            label1.TabIndex = 30;
+            // 
+            // lblValor
+            // 
+            lblValor.AutoSize = true;
+            lblValor.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblValor.ForeColor = Color.FromArgb(63, 136, 254);
+            lblValor.Location = new Point(455, 101);
+            lblValor.Name = "lblValor";
+            lblValor.Size = new Size(59, 25);
+            lblValor.TabIndex = 31;
+            lblValor.Text = "Valor";
+            // 
+            // lblCliente
+            // 
+            lblCliente.AutoSize = true;
+            lblCliente.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCliente.ForeColor = Color.FromArgb(63, 136, 254);
+            lblCliente.Location = new Point(221, 101);
+            lblCliente.Name = "lblCliente";
+            lblCliente.Size = new Size(73, 25);
+            lblCliente.TabIndex = 32;
+            lblCliente.Text = "Cliente";
+            // 
+            // lblProduto
+            // 
+            lblProduto.AutoSize = true;
+            lblProduto.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblProduto.ForeColor = Color.FromArgb(63, 136, 254);
+            lblProduto.Location = new Point(12, 101);
+            lblProduto.Name = "lblProduto";
+            lblProduto.Size = new Size(87, 25);
+            lblProduto.TabIndex = 33;
+            lblProduto.Text = "Produto";
+            // 
+            // btnVoltar
+            // 
+            btnVoltar.BackColor = Color.FromArgb(72, 116, 245);
+            btnVoltar.BackgroundColor = Color.FromArgb(72, 116, 245);
+            btnVoltar.BorderColor = Color.FromArgb(0, 242, 254);
+            btnVoltar.BorderRadius = 14;
+            btnVoltar.BorderSize = 2;
+            btnVoltar.FlatAppearance.BorderSize = 0;
+            btnVoltar.FlatStyle = FlatStyle.Flat;
+            btnVoltar.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Pixel);
+            btnVoltar.ForeColor = Color.Black;
+            btnVoltar.Location = new Point(677, 12);
+            btnVoltar.Name = "btnVoltar";
+            btnVoltar.Size = new Size(111, 31);
+            btnVoltar.TabIndex = 34;
+            btnVoltar.Text = "Voltar";
+            btnVoltar.TextColor = Color.Black;
+            btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // btnImprimir
+            // 
+            btnImprimir.BackColor = Color.FromArgb(72, 116, 245);
+            btnImprimir.BackgroundColor = Color.FromArgb(72, 116, 245);
+            btnImprimir.BorderColor = Color.FromArgb(0, 242, 254);
+            btnImprimir.BorderRadius = 14;
+            btnImprimir.BorderSize = 2;
+            btnImprimir.FlatAppearance.BorderSize = 0;
+            btnImprimir.FlatStyle = FlatStyle.Flat;
+            btnImprimir.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnImprimir.ForeColor = Color.Black;
+            btnImprimir.Location = new Point(1, 407);
+            btnImprimir.Name = "btnImprimir";
+            btnImprimir.Size = new Size(111, 31);
+            btnImprimir.TabIndex = 35;
+            btnImprimir.Text = "IMPRIMIR";
+            btnImprimir.TextColor = Color.Black;
+            btnImprimir.UseVisualStyleBackColor = false;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.BackColor = Color.FromArgb(72, 116, 245);
+            btnSalvar.BackgroundColor = Color.FromArgb(72, 116, 245);
+            btnSalvar.BorderColor = Color.FromArgb(0, 242, 254);
+            btnSalvar.BorderRadius = 14;
+            btnSalvar.BorderSize = 2;
+            btnSalvar.FlatAppearance.BorderSize = 0;
+            btnSalvar.FlatStyle = FlatStyle.Flat;
+            btnSalvar.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSalvar.ForeColor = Color.Black;
+            btnSalvar.Location = new Point(158, 407);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(111, 31);
+            btnSalvar.TabIndex = 36;
+            btnSalvar.Text = "SALVAR";
+            btnSalvar.TextColor = Color.Black;
+            btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // EmissaoRelatorioFinanceiroProdutos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(16, 25, 56);
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalvar);
+            Controls.Add(btnImprimir);
+            Controls.Add(btnVoltar);
+            Controls.Add(lblProduto);
+            Controls.Add(lblCliente);
+            Controls.Add(lblValor);
+            Controls.Add(label1);
+            Controls.Add(lblAcao);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "EmissaoRelatorioFinanceiroProdutos";
+            Text = "EmissaoRelatorioFinanceiroProdutos";
+            Load += EmissaoRelatorioFinanceiroProdutos_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblAcao;
+        private Label label1;
+        private Label lblValor;
+        private Label lblCliente;
+        private Label lblProduto;
+        private WiLBiT.WiLBiTButton2 btnVoltar;
+        private WiLBiT.WiLBiTButton2 btnImprimir;
+        private WiLBiT.WiLBiTButton2 btnSalvar;
+    }
+}
