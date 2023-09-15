3b9cb12f385f92b4d6be47c06e96a1adfe063eaa
Eduardo_Lira <135743538+Edulira14@users.noreply.github.com>
2023-09-15 15:41:30 -0300

    Add files via upload

---
+++ PixeLogicBDPlus.sql
@@ -0,0 +1,260 @@
+CREATE DATABASE  IF NOT EXISTS `pixelogic` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `pixelogic`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pixelogic
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cadastro_nova_conta`
+--
+
+DROP TABLE IF EXISTS `cadastro_nova_conta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cadastro_nova_conta` (
+  `nome` varchar(125) NOT NULL,
+  `empresa` varchar(60) NOT NULL,
+  `e-mail` varchar(100) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `senha` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cadastro_nova_conta`
+--
+
+LOCK TABLES `cadastro_nova_conta` WRITE;
+/*!40000 ALTER TABLE `cadastro_nova_conta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cadastro_nova_conta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `nome` varchar(100) NOT NULL,
+  `endereco` varchar(200) NOT NULL,
+  `cpf` int(20) NOT NULL,
+  `telefone` int(20) NOT NULL,
+  `cep` int(20) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `numero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fornecedores`
+--
+
+DROP TABLE IF EXISTS `fornecedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fornecedores` (
+  `nome` varchar(100) NOT NULL,
+  `endereco` varchar(150) NOT NULL,
+  `telefone` int(20) NOT NULL,
+  `cnpj` int(20) NOT NULL,
+  `cep` int(20) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `cidade` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fornecedores`
+--
+
+LOCK TABLES `fornecedores` WRITE;
+/*!40000 ALTER TABLE `fornecedores` DISABLE KEYS */;
+INSERT INTO `fornecedores` VALUES ('eduardo','Avenida São Carlos',1993781740,123488899,13567888,12,'São Carlos');
+/*!40000 ALTER TABLE `fornecedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcoes`
+--
+
+DROP TABLE IF EXISTS `funcoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcoes` (
+  `nome` varchar(40) NOT NULL,
+  `habilitado_nao` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcoes`
+--
+
+LOCK TABLES `funcoes` WRITE;
+/*!40000 ALTER TABLE `funcoes` DISABLE KEYS */;
+INSERT INTO `funcoes` VALUES ('função de fornecedores',1),('função de produtos',1),('função de clientes',1),('função de serviços',1),('função de vendas',1),('função de relatorio_produtos',1),('função de relatorio_serviços',0),('função de suporte',1),('função de colaboradores',1);
+/*!40000 ALTER TABLE `funcoes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orcamentos`
+--
+
+DROP TABLE IF EXISTS `orcamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orcamentos` (
+  `servico` varchar(60) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `nome_cliente` varchar(100) NOT NULL,
+  `endereco` varchar(150) NOT NULL,
+  `funcionario` varchar(100) NOT NULL,
+  `estimativa_entrega` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orcamentos`
+--
+
+LOCK TABLES `orcamentos` WRITE;
+/*!40000 ALTER TABLE `orcamentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orcamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `nome` varchar(100) NOT NULL,
+  `fornecedor` varchar(100) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `vencimento` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicos`
+--
+
+DROP TABLE IF EXISTS `servicos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicos` (
+  `nome_servico` varchar(100) NOT NULL,
+  `valor` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicos`
+--
+
+LOCK TABLES `servicos` WRITE;
+/*!40000 ALTER TABLE `servicos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `nome` varchar(100) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `senha` text NOT NULL,
+  `cargo` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES ('joao','joao@desenvolvedor','1234','desenvolvedor'),('jhonnes','jhonnes@usuario','1234','chefe de cozinha');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vendas`
+--
+
+DROP TABLE IF EXISTS `vendas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vendas` (
+  `produto` varchar(60) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `nome_cliente` varchar(120) NOT NULL,
+  `metodo_pagamento` varchar(30) NOT NULL,
+  `valor` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vendas`
+--
+
+LOCK TABLES `vendas` WRITE;
+/*!40000 ALTER TABLE `vendas` DISABLE KEYS */;
+INSERT INTO `vendas` VALUES ('camisetas',10,'João','cartao debito',25.00),('chinelo',10,'joao','dinheiro',100.00),('chinelo',10,'joao','dinheiro',100.00);
+/*!40000 ALTER TABLE `vendas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'pixelogic'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-15 15:39:38
