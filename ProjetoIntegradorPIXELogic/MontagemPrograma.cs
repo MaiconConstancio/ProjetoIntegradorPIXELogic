9fa64d866f1678a9da9daf93555a2d56461bc650
JoaoVictorBento <joao.vbsantos37@senacsp.edu.br>
2023-09-16 21:30:03 -0300

    erro de montagem corrigido

--- ProjetoIntegradorPIXELogic/MontagemPrograma.cs
+++ ProjetoIntegradorPIXELogic/MontagemPrograma.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,126 +26,189 @@ namespace ProjetoIntegradorPIXELogic
             if (chkBoxGerenciamentoServiços.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de serviços';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'genciamento servico'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'gerenciamento servico';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('gerenciamento servico', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de serviços';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'gerenciamento serviços';");
 
             }
 
             if (chkBoxClientes.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de clientes';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'clientes'").Rows.Count == 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'clientes';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('clientes', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de clientes';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'clientes';");
 
             }
 
             if (chkBoxProdutos.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de produtos';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'produtos'").Rows.Count == 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'produtos';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('produtos', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de produtos';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'produtos';");
 
             }
 
             if (chkBoxFornecedores.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de fornecedores';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'fornecedores'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'fornecedores';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('fornecedores', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de fornecedores';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'fornecedores';");
 
             }
 
             if (chkBoxColaboradores.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de colaboradores';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'colaboradores'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'colaboradores';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('colaboradores', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de colaboradores';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'colaboradores';");
 
             }
 
             if (chkBoxEmissaoRelatoriosFinanceiros.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de relatorio_produtos';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'relatorio de produtos'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'relatorio de produtos';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('relatorio de produtos', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de relatorio_produtos';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'relatorio de produtos';");
 
             }
 
             if (checkBox1.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de relatorio_serviços';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'relatorio de servicos'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'relatorio de servicos';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('relatorio de servicos', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de relatorio_serviços';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'relatorio de servicos';");
 
             }
 
             if (chkBoxVendas.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de vendas';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'vendas'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'vendas';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('vendas', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de vendas';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'vendas';");
 
             }
 
             if (chkBoxSuporteAoCliente.Checked == true)
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'função de suporte';");
+                if (Conexao.executaQuery("select * from funcoes where nome = 'suporte'").Rows.Count > 0)
+                {
+
+                    Conexao.executaQuery("update funcoes set habilitado_nao = true where nome = 'suporte';");
+
+                }
+
+                else { Conexao.executaQuery("insert into funcoes (nome,habilitado_nao) values ('suporte', true)"); }
 
             }
 
             else
             {
 
-                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'função de suporte';");
+                Conexao.executaQuery("update funcoes set habilitado_nao = false where nome = 'suporte';");
 
             }
 
@@ -159,56 +223,56 @@ namespace ProjetoIntegradorPIXELogic
         private void MontagemPrograma_Load(object sender, EventArgs e)
         {
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de serviços' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'servicos' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxGerenciamentoServiços.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de clientes' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'clientes' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxClientes.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de produtos' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'produtos' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxProdutos.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de fornecedores' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'fornecedores' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxFornecedores.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de colaboradores' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'colaboradores' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxColaboradores.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de vendas' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'vendas' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxVendas.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de relatorio_produtos' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'relatorio de produtos' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxEmissaoRelatoriosFinanceiros.Checked = true;
 
             }
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de relatorio_serviços' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'relatorio de servicos' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 checkBox1.Checked = true;
@@ -216,7 +280,7 @@ namespace ProjetoIntegradorPIXELogic
             }
 
 
-            if (Conexao.executaQuery("select * from funcoes where nome = 'função de suporte' and habilitado_nao = true;").Rows.Count > 0)
+            if (Conexao.executaQuery("select * from funcoes where nome = 'suporte' and habilitado_nao = true;").Rows.Count > 0)
             {
 
                 chkBoxSuporteAoCliente.Checked = true;
